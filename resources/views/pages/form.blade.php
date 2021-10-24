f43e50e1c855e11ad322d346d9242d60ee4cf87e
Jerry Manurung <jerry.voldo@gmail.com>
2021-10-24 23:39:35 +0700

    fix sbbk page

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -7,11 +7,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div>
+            <div class="flex flex-row gap-4">
+                <div class=" text-center p-6 bg-white border-b border-gray-200 hover:bg-green-400 hover:text-white hover:font-bold rounded">
+                    <div class="">
                         <a href="{{ route('form.spb') }}">Surat Pemesanan Barang</a>
                     </div>
+                </div>
+                <div class="text-center p-6 bg-white border-b border-gray-200 hover:bg-purple-400 hover:text-white hover:font-bold rounded">
                     <div>
                         <a href="{{ route('form.sbbk') }}">Surat Bukti Barang Keluar</a>
                     </div>
